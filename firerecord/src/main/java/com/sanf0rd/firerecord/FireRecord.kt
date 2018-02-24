34afc850b02be1b0554284c29a03b827383772da
David Sanford <dsanfordseu@gmail.com>
2018-02-24 19:30:04 -0300

    Add package level computed property as 'type-alias' to Firestore db

--- firerecord/src/main/java/com/sanf0rd/firerecord/FireRecord.kt
+++ firerecord/src/main/java/com/sanf0rd/firerecord/FireRecord.kt
@@ -7,6 +7,9 @@ import com.google.firebase.firestore.FirebaseFirestore
  * Created by sanf0rd on 24/02/18.
  */
 
+val firestore: FirebaseFirestore
+    get() = FirebaseFirestore.getInstance()
+
 open class FireRecord {
 
     fun save() {
