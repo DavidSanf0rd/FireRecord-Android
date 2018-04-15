f028c181f31c44478a0f55bff563eeaf5a3f6f46
David Sanford <dsanfordseu@gmail.com>
2018-04-15 09:49:44 -0300

    Add basic equal to infix comparator to use as parameter

--- firerecord/src/main/java/com/sanf0rd/firerecord/FireRecordQuery.kt
+++ firerecord/src/main/java/com/sanf0rd/firerecord/FireRecordQuery.kt
@@ -12,3 +12,7 @@ enum class QueryType {
 
 class FireRecordComparisonQuery<R: FireRecord>(val prop: KMutableProperty1<R ,out Int?>,
                                                val value: Int, val type: QueryType)
+
+infix fun <T: FireRecord> KMutableProperty1<T, out Int?>.equalTo(value: Int): FireRecordComparisonQuery<T> {
+    return FireRecordComparisonQuery(this, value, QueryType.EqualTo)
+}
