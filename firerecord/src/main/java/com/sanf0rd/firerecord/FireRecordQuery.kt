1e8482f321b9133dd311875c825db9e5fae6c584
David Sanford <dsanfordseu@gmail.com>
2018-04-17 16:21:08 -0300

    Add a query list and prepare to evaluate it

--- firerecord/src/main/java/com/sanf0rd/firerecord/FireRecordQuery.kt
+++ firerecord/src/main/java/com/sanf0rd/firerecord/FireRecordQuery.kt
@@ -1,14 +1,23 @@
 package com.sanf0rd.firerecord
 
-import kotlin.reflect.KMutableProperty1
-
 fun <T: FireRecordCompanion<R>, R: FireRecord> T.where(query: FireRecordComparisonQuery<R>): FireRecordQuery<R> {
     print(query.value)
     return FireRecordQuery()
 }
 
 class FireRecordQuery<T: FireRecord> {
+    private val queryList = mutableListOf<FireRecordComparisonQuery<T>>()
+
     fun where(query: FireRecordComparisonQuery<T>): FireRecordQuery<T> {
-        return FireRecordQuery()
+        queryList.add(query)
+        return this
+    }
+
+    fun take(){
+
+    }
+
+    fun takeAll() {
+
     }
 }
