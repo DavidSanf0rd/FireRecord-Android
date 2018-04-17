d94f3e643bca7a473d15b214dd99d6f057d25c21
David Sanford <dsanfordseu@gmail.com>
2018-04-17 17:32:40 -0300

    Add basic evaluate method to fireRecord query

--- firerecord/src/main/java/com/sanf0rd/firerecord/FireRecordQuery.kt
+++ firerecord/src/main/java/com/sanf0rd/firerecord/FireRecordQuery.kt
@@ -1,11 +1,13 @@
 package com.sanf0rd.firerecord
 
-fun <T: FireRecordCompanion<R>, R: FireRecord> T.where(query: FireRecordComparisonQuery<R>): FireRecordQuery<R> {
+import kotlin.reflect.KClass
+
+inline fun <T: FireRecordCompanion<R>,reified R: FireRecord> T.where(query: FireRecordComparisonQuery<R>): FireRecordQuery<R> {
     print(query.value)
-    return FireRecordQuery()
+    return FireRecordQuery(R::class).where(query)
 }
 
-class FireRecordQuery<T: FireRecord> {
+class FireRecordQuery<T: FireRecord>(private val kClass: KClass<T>) {
     private val queryList = mutableListOf<FireRecordComparisonQuery<T>>()
 
     fun where(query: FireRecordComparisonQuery<T>): FireRecordQuery<T> {
@@ -13,7 +15,20 @@ class FireRecordQuery<T: FireRecord> {
         return this
     }
 
-    fun take(){
+    private fun evaluate() {
+        val collectionRef = firestore.collection("/${kClass.java.simpleName.toLowerCase()}")
+        var firestoreQuery = queryList.first().buildFirstQuery(collectionRef)
+
+        firestoreQuery = queryList.subList(1, queryList.size).fold(firestoreQuery) {
+            currentQuery, fireRecordQuery -> fireRecordQuery.buildFirestoreQuery(currentQuery)
+        }
+
+        firestoreQuery.get().addOnCompleteListener {
+            print(it)
+        }
+    }
+
+    fun take() {
 
     }
 
