42873c22250aa10cb90412f8c4a3cbbb88765c57
David Sanford <dsanfordseu@gmail.com>
2018-04-17 17:31:57 -0300

    Build firestore query with current fireRecord query infra structure

--- firerecord/src/main/java/com/sanf0rd/firerecord/FireRecordComparisonQuery.kt
+++ firerecord/src/main/java/com/sanf0rd/firerecord/FireRecordComparisonQuery.kt
@@ -1,10 +1,30 @@
 package com.sanf0rd.firerecord
 
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.Query
+import com.sanf0rd.firerecord.QueryType.*
 import kotlin.reflect.KMutableProperty1
 
-class FireRecordComparisonQuery<R: FireRecord>(val prop: KMutableProperty1<R, out Int?>,
-                                               val value: Int, val type: QueryType)
+class FireRecordComparisonQuery<R : FireRecord>(val prop: KMutableProperty1<R, out Int?>,
+                                                val value: Int, val type: QueryType) {
 
-infix fun <T: FireRecord> KMutableProperty1<T, out Int?>.equalTo(value: Int): FireRecordComparisonQuery<T> {
-    return FireRecordComparisonQuery(this, value, QueryType.EqualTo)
+    fun buildFirestoreQuery(currentQuery: Query) = when (type) {
+        EqualTo -> currentQuery.whereEqualTo(prop.name, value)
+        GreaterThan -> currentQuery.whereGreaterThan(prop.name, value)
+        GreaterThanOrEqualTo -> currentQuery.whereGreaterThanOrEqualTo(prop.name, value)
+        LessThanOrEqualTo -> currentQuery.whereLessThanOrEqualTo(prop.name, value)
+        LessThan -> currentQuery.whereLessThan(prop.name, value)
+    }
+
+    fun buildFirstQuery(collectionRef: CollectionReference) = when (type) {
+        EqualTo -> collectionRef.whereEqualTo(prop.name, value)
+        GreaterThan -> collectionRef.whereGreaterThan(prop.name, value)
+        GreaterThanOrEqualTo -> collectionRef.whereGreaterThanOrEqualTo(prop.name, value)
+        LessThanOrEqualTo -> collectionRef.whereLessThanOrEqualTo(prop.name, value)
+        LessThan -> collectionRef.whereLessThan(prop.name, value)
+    }
+}
+
+infix fun <T : FireRecord> KMutableProperty1<T, out Int?>.equalTo(value: Int): FireRecordComparisonQuery<T> {
+    return FireRecordComparisonQuery(this, value, EqualTo)
 }
--- firerecord/src/main/java/com/sanf0rd/firerecord/QueryType.kt
+++ firerecord/src/main/java/com/sanf0rd/firerecord/QueryType.kt
@@ -1,9 +1,12 @@
 package com.sanf0rd.firerecord
 
+import com.google.firebase.firestore.Query
+import com.sanf0rd.firerecord.QueryType.*
+
 enum class QueryType {
     EqualTo,
     GreaterThan,
     GreaterThanOrEqualTo,
     LessThanOrEqualTo,
-    LessThan
+    LessThan;
 }
