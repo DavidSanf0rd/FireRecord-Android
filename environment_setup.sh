7b3530ea0efe51e520cabc174ebc00cec2b1efa0
David Sanford <dsanfordseu@gmail.com>
2018-10-22 23:37:21 -0300

    Add enviroment_setup.sh

---
+++ environment_setup.sh
@@ -0,0 +1,35 @@
+#
+#           Circle CI & gradle.properties live in harmony
+#
+# Android convention is to store your API keys in a local, non-versioned
+# gradle.properties file. Circle CI doesn't allow users to upload pre-populated
+# gradle.properties files to store this secret information, but instead allows
+# users to store such information as environment variables.
+#
+# You must execute this script via your circle.yml as a pre-process dependency,
+# so your gradle build process has access to all variables.
+#
+#   dependencies:
+#       pre:
+#        - source environmentSetup.sh && copyEnvVarsToGradleProperties
+
+#!/usr/bin/env bash
+
+function copyEnvVarsToGradleProperties {
+    GRADLE_PROPERTIES=$HOME"/.gradle/gradle.properties"
+    export GRADLE_PROPERTIES
+    echo "Gradle Properties should exist at $GRADLE_PROPERTIES"
+
+    if [ ! -f "$GRADLE_PROPERTIES" ]; then
+        echo "Gradle Properties does not exist"
+
+        echo "Creating Gradle Properties file..."
+        touch $GRADLE_PROPERTIES
+
+        echo "Writing TEST_API_KEY to gradle.properties..."
+        echo "bintray_user=$BINTRAY_USER" >> $GRADLE_PROPERTIES
+        echo "bintray_apikey=$BINTRAY_APIKEY" >> $GRADLE_PROPERTIES
+        echo "bintray_gpg_pass=$BINTRAY_GPG_PASS" >> $GRADLE_PROPERTIES
+
+    fi
+}
