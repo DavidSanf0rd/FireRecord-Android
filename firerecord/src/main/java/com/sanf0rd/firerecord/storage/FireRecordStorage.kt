812d0435683742ffdd3d2203f000cd615fed856c
David Sanford <dsanfordseu@gmail.com>
2019-09-14 19:08:20 -0300

    Fix and upgrade dependencies

--- firerecord/src/main/java/com/sanf0rd/firerecord/storage/FireRecordStorage.kt
+++ firerecord/src/main/java/com/sanf0rd/firerecord/storage/FireRecordStorage.kt
@@ -1,7 +1,11 @@
 package com.sanf0rd.firerecord.storage
 
+import android.graphics.Bitmap
+import com.google.android.gms.tasks.OnFailureListener
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import com.sanf0rd.firerecord.FireRecord
+import java.io.ByteArrayOutputStream
 
 val firebaseStorage = FirebaseStorage.getInstance()
 
@@ -14,7 +18,19 @@ class FireRecordStorage {
             instanceClass.declaredFields.forEach { field ->
                 if (field.type.superclass.isAssignableFrom(FirebaseFile::class.java)) {
                     println(field)
+                    sendImageToFirebase(field.get(instance))
                 }
+            }
+        }
+
+        fun sendImageToFirebase(file: FirebaseFile) {
+            val imageRef = firebaseStorage.reference.child("mountains.jpg")
+
+            val uploadTask = imageRef.putBytes(file.data!!)
+            uploadTask.addOnFailureListener { error ->
+                print(error)
+            }.addOnSuccessListener { snapshot ->
+                print(snapshot)
             }
         }
     }
